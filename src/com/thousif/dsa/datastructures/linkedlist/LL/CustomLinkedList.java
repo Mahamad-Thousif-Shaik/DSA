5fb51dcf33402da65f47d102676c35a1abc0e069
thousif <thousif7896@gmail.com>
2025-09-12 23:53:56 +0530

    Implemented some methods of Linked List.

--- src/com/thousif/dsa/datastructures/linkedlist/LL/CustomLinkedList.java
+++ src/com/thousif/dsa/datastructures/linkedlist/LL/CustomLinkedList.java
@@ -1,5 +1,7 @@
 package com.thousif.dsa.datastructures.linkedlist.LL;
 
+import java.util.NoSuchElementException;
+
 public class CustomLinkedList<T> {
 
     private Node head;
@@ -7,44 +9,44 @@ public class CustomLinkedList<T> {
 
     private int size;
 
-    private class Node{
+    private class Node {
         private T value;
         private Node next;
 
-        Node(T value){
+        Node(T value) {
             this.value = value;
         }
     }
 
-    public void print(){
+    public void print() {
         Node current = head;
-        while(current != null){
+        while (current != null) {
             System.out.print(current.value + " ");
             current = current.next;
         }
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void add(T value){
+    public void add(T value) {
         insertLast(value);
     }
 
-    public void insertFirst(T value){
+    public void insertFirst(T value) {
         Node newNode = new Node(value);
         newNode.next = head;
         head = newNode;
-        if(tail == null){
+        if (tail == null) {
             tail = head;
         }
         size++;
     }
 
-    public void insertLast(T value){
+    public void insertLast(T value) {
         Node newNode = new Node(value);
-        if(tail == null){
+        if (tail == null) {
             insertFirst(value);
             return;
         }
@@ -52,6 +54,61 @@ public class CustomLinkedList<T> {
         tail = newNode;
         size++;
     }
+
+    public void add(int index, T value) {
+        if (index == 0) {
+            insertFirst(value);
+        } else if (index == size) {
+            insertLast(value);
+        } else {
+            if (index < 0 || index > size) {
+                throw new IllegalArgumentException("Index out of bound exception: " + index);
+            } else {
+                Node newNode = new Node(value);
+                Node currentNode = head;
+                int count = 1;
+                while (count < index) {
+                    currentNode = currentNode.next;
+                    count++;
+                }
+                newNode.next = currentNode.next;
+                currentNode.next = newNode;
+                size++;
+            }
+        }
+    }
+
+    public T deleteFirst(){
+        if(head == null){
+            throw new NoSuchElementException("List is empty");
+        }
+        else{
+            T value = head.value;
+            head = head.next;
+            size--;
+            return value;
+        }
+    }
+
+    public T deleteLast(){
+        if(tail == null){
+            throw new NoSuchElementException("List is empty");
+        }
+        else{
+            T value = tail.value;
+            Node currentNode = head;
+            for(int i=1; i<size-1; i++){
+                currentNode = currentNode.next;
+            }
+            tail = currentNode;
+            currentNode.next = null;
+            size--;
+            return value;
+        }
+
+
+    }
+
 
 }
 
--- src/com/thousif/dsa/datastructures/linkedlist/LL/Main.java
+++ src/com/thousif/dsa/datastructures/linkedlist/LL/Main.java
@@ -4,12 +4,22 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        
         CustomLinkedList<Integer> list = new CustomLinkedList<>();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.insertFirst(0);
+        list.insertLast(6);
+        list.add(5, 5);
+        System.out.println(list.deleteFirst());
         list.print();
+        System.out.println();
+        System.out.println(list.size());
+        System.out.println(list.deleteLast());
+        list.print();
+        System.out.println();
+        System.out.println(list.size());
     }
 }
